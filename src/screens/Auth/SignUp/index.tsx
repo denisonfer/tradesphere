d1dbfa07830bdf1b8fce65ed325e0ec7faaab3f9
denisonfer <denisonferreiram91@gmail.com>
2024-06-30 01:35:36 -0300

    feat: form validation

--- src/screens/Auth/SignUp/index.tsx
+++ src/screens/Auth/SignUp/index.tsx
@@ -1,14 +1,53 @@
 import Button from '@components/Button';
 import Input from '@components/Input';
 import { Center, Heading, Text, VStack } from 'native-base';
-import React from 'react';
+import React, { useCallback } from 'react';
 
 import Logo from '@assets/images/logo.svg';
+import { yupResolver } from '@hookform/resolvers/yup';
 import { useNavigation } from '@react-navigation/native';
+import { Controller, useForm } from 'react-hook-form';
 import { KeyboardAwareScrollView } from 'react-native-keyboard-aware-scroll-view';
+import * as yup from 'yup';
+
+type TFormData = {
+  name: string;
+  email: string;
+  phone: string;
+  password: string;
+  confirmPass: string;
+};
+
+const signUpSchema = yup.object().shape({
+  name: yup.string().required('Nome obrigatório'),
+  email: yup.string().required('Email obrigatório').email('Email inválido'),
+  phone: yup
+    .string()
+    .required('Telefone obrigatório')
+    .matches(/^\(\d{2}\) 9\d{4}-\d{4}$/, 'Telefone inválido'),
+  password: yup
+    .string()
+    .required('Senha obrigatória')
+    .min(6, 'No mínimo 6 dígitos'),
+  confirmPass: yup
+    .string()
+    .required('Confirme sua senha')
+    .oneOf([yup.ref('password')], 'Senhas diferentes'),
+});
 
 const SignUp: React.FC = () => {
   const { goBack } = useNavigation();
+
+  const {
+    control,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<TFormData>({
+    resolver: yupResolver(signUpSchema),
+  });
+
+  const handleSignUp = useCallback((data: TFormData) => {}, []);
+
   return (
     <KeyboardAwareScrollView>
       <VStack flex={1}>
@@ -30,16 +69,80 @@ const SignUp: React.FC = () => {
           <Text mt={16} mb={4}>
             Acesse sua conta
           </Text>
-          <Input placeholder='Nome' />
-          <Input
-            placeholder='E-mail'
-            keyboardType='email-address'
-            autoCapitalize='none'
+          <Controller
+            control={control}
+            name='name'
+            render={({ field: { onChange, value } }) => (
+              <Input
+                placeholder='Nome'
+                onChangeText={onChange}
+                value={value}
+                errorMessage={errors.name?.message}
+              />
+            )}
           />
-          <Input placeholder='Telefone' />
-          <Input placeholder='Senha' secureTextEntry />
-          <Input placeholder='Confirmar senha' secureTextEntry />
-          <Button title='Criar' bgColor='gray.100' isFullWidth></Button>
+          <Controller
+            control={control}
+            name='email'
+            render={({ field: { onChange, value } }) => (
+              <Input
+                placeholder='E-mail'
+                keyboardType='email-address'
+                autoCapitalize='none'
+                onChangeText={onChange}
+                value={value}
+                errorMessage={errors.email?.message}
+              />
+            )}
+          />
+
+          <Controller
+            control={control}
+            name='phone'
+            render={({ field: { onChange, value } }) => (
+              <Input
+                placeholder='Telefone'
+                onChangeText={onChange}
+                value={value}
+                errorMessage={errors.phone?.message}
+              />
+            )}
+          />
+          <Controller
+            control={control}
+            name='password'
+            render={({ field: { onChange, value } }) => (
+              <Input
+                placeholder='Senha'
+                secureTextEntry
+                onChangeText={onChange}
+                value={value}
+                errorMessage={errors.password?.message}
+              />
+            )}
+          />
+          <Controller
+            control={control}
+            name='confirmPass'
+            render={({ field: { onChange, value } }) => (
+              <Input
+                placeholder='Confirmar senha'
+                secureTextEntry
+                returnKeyType='send'
+                onSubmitEditing={handleSubmit(handleSignUp)}
+                onChangeText={onChange}
+                value={value}
+                errorMessage={errors.confirmPass?.message}
+              />
+            )}
+          />
+
+          <Button
+            title='Criar'
+            bgColor='gray.100'
+            isFullWidth
+            onPress={handleSubmit(handleSignUp)}
+          ></Button>
         </Center>
 
         <Center mt={16} px={10}>
